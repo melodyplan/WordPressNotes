c2c0296c57b2b33702adee1b4b7a36d94e483eec
Melody Plan <melodyplan@gmail.com>
2017-12-29 16:05:10 -0800

    added content page updated others to be more DRY
    added content page and edited index archive and search to be more DRY

--- archive.php
+++ archive.php
@@ -32,40 +32,11 @@ if (have_posts()) :
   ?></h2>
 
   <?php
-  while (have_posts()) : the_post(); ?>
+  while (have_posts()) : the_post();
 
-  <article class="post">
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  get_template_part('content');
 
-    <p class="post-info">
-      <!-- google php date strings for how you want to format the time/date -->
-      <?php the_time('F jS, Y g:i a'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author() ?></a> | Posted in
-      <?php
-        $categories = get_the_category();
-        $separator = ", ";
-        $output = '';
-
-        if ($categories) {
-
-          foreach ($categories as $category) {
-
-            // ```.=``` adds on to a variable instead of overwriting it, so in this case it adds on another category instead of replaces the first one it finds.
-            $output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
-
-          }
-
-          //this tells it to remove the comma after the final category
-          echo trim($output, $separator);
-
-        }
-      ?>
-    </p>
-
-    <!-- if you change the_post() to the_excerpt() it will only show the first paragraph or the excerpt of the post and when you click the post it will then show the entire post, which makes for cleaner archiving. -->
-    <?php the_post(); ?>
-  </article>
-
-<?php endwhile;
+ endwhile;
 
   else :
     echo '<p>No content found</p>';
