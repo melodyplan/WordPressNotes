7f046486f5c693e68054261a6c7d3e57a10b8ed3
Melody Plan <melodyplan@gmail.com>
2017-12-30 13:00:48 -0800

    added a static front page with custom template

---
+++ front-page.php
@@ -0,0 +1,31 @@
+<!-- Controls Home page -->
+<?php
+
+get_header(); ?>
+
+  <!-- site-content -->
+  <div class="site-content clearfix">
+
+    <h3>Custom HTML here!</h3>
+
+      <?php if (have_posts()) :
+        while (have_posts()) : the_post();
+
+          the_content();
+
+        endwhile;
+
+        else :
+          echo '<p>No content found</p>';
+
+        endif;
+        ?>
+
+      <h3>Custom HTML here!</h3>
+
+
+  </div><!-- /site-content -->
+
+<?php get_footer();
+
+?>
