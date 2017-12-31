faa80fbe37c87c0f336fa335e4a8c0b34e7585cc
Melody Plan <melodyplan@gmail.com>
2017-12-30 18:09:32 -0800

    creating custom loops in wp query

--- front-page.php
+++ front-page.php
@@ -19,10 +19,60 @@ get_header(); ?>
           echo '<p>No content found</p>';
 
         endif;
+
         ?>
 
-      <h3>Custom HTML here!</h3>
+        <!-- home-columns -->
+        <div class="home-columns clearfix">
 
+          <!-- one-half -->
+          <div class="one-half">
+            <?php
+            // opinion posts loop begins here
+            $opinionPosts = new WP_Query('cat=4&posts_per_page=2');
+
+            if ($opinionPosts -> have_posts()) :
+
+              while ($opinionPosts -> have_posts()) : $opinionPosts->the_post(); ?>
+                 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                 <?php the_excerpt(); ?>
+              <?php endwhile;
+
+              else:
+
+            endif;
+            /*prevents custom WP_Query routes from disrupting the main natural
+            URL-based loops that WordPress runs on its own. So the function below is
+            basically saying "okay WordPress we've done the custom loop, we're
+            reliquishing control back to you"*/
+            wp_reset_postdata();
+            ?>
+          </div>
+          <!-- /one-half -->
+
+          <!-- one-half -->
+          <div class="one-half last">
+            <?php
+            // news posts loop begins here
+            $informationalPosts = new WP_Query('cat=5&posts_per_page=2');
+
+            if ($informationalPosts -> have_posts()) :
+
+              while ($informationalPosts -> have_posts()) : $informationalPosts->the_post(); ?>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <?php the_excerpt(); ?>
+              <?php endwhile;
+
+              else:
+
+            endif;
+            wp_reset_postdata();
+            ?>
+          </div>
+          <!-- /one-half -->
+
+        </div>
+        <!-- /home-columns -->
 
   </div><!-- /site-content -->
 
