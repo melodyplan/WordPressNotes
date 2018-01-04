d2457c295d95ab99b51dc8079e61397df2d57714
Melody Plan <melodyplan@gmail.com>
2018-01-04 08:16:03 -0800

    added about the author sections
    added about the author sections to individual posts

--- single.php
+++ single.php
@@ -38,9 +38,50 @@ if (have_posts()) :
 
   </article> */
 
-get_template_part('content', get_post_format());
+get_template_part('content', get_post_format());?>
 
-endwhile;
+<div class="about-author clearfix">
+  <div class="about-author-image">
+    <?php echo get_avatar(get_the_author_meta('ID'), 512) ?>
+    <p><?php echo get_the_author_meta('nickname') ?></p>
+  </div>
+
+  <?php $otherAuthorPosts = new WP_Query(array(
+    'author' => get_the_author_meta('ID'),
+    'posts_per_page' => 3,
+    // excludes certain posts ID from the results of this search query
+    'posts__not_in' => array(get_the_ID())
+  )); ?>
+
+  <div class="about-author-text">
+    <h3>About the Author</h3>
+    <?php echo wpautop(get_the_author_meta('description')) ?>
+
+    <?php if ($otherAuthorPosts->have_posts()) { ?>
+    <div class="other-posts-by">
+      <h4>Other posts by <?php echo get_the_author_meta('nickname') ?></h4>
+      <ul>
+        <?php while ($otherAuthorPosts->have_posts()) {
+          $otherAuthorPosts->the_post(); ?>
+        <li><a href="<?php the_permalink() ?>"><?php the_title() ?></a></li>
+        <?php } ?>
+      </ul>
+
+    </div>
+    <?php } wp_reset_postdata ?>
+
+    <?php if (count_user_posts(get_the_author_meta('ID')) > 3) { ?>
+    <a class="btn-a" href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>">
+      View all posts by <?php echo get_the_author_meta('nickname') ?>
+    </a>
+    <?php } ?>
+
+  </div>
+</div>
+
+</div>
+
+<?php endwhile;
 
   else :
     echo '<p>No content found</p>';
